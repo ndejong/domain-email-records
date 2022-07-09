b12f58caa85bc7abbed3698a6f081a9683b3a0c9
Nicholas de Jong <ndejong@threatpatrols.com>
2022-07-09 17:40:06 +1000

    (domain-email-records) bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requirements = [
 
 setuptools.setup(
   name = 'domain-email-records',
-  version = '0.3.0',
+  version = '0.3.1',
   author = 'Nicholas de Jong',
   author_email = 'ndejong@threatpatrols.com',
   description = 'CLI tool to quickly lookup MX, SPF, DMARC records for many domains',
