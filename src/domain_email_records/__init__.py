11a353e88ba2d096f7e64a4d1137e34bb502c4ea
Nicholas de Jong <ndejong@threatpatrols.com>
2023-10-20 21:00:52 +1000

    release 0.5.0 Migrate to slapcli and remove old `slamcli` build artifacts Apply mypy testing and updates Fix bug with `lookup_types` ever extending "mx_preference" records Rearrange files into `lib` dir Black formatting Bump dependency versions Replace Logger Update changelog

--- src/domain_email_records/__init__.py
+++ src/domain_email_records/__init__.py
@@ -3,7 +3,5 @@
 
 __title__ = "domain-email-records"
 __author__ = "Nicholas de Jong <ndejong@threatpatrols.com>"
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 __license__ = "BSD2"
-
-DOMAIN_RECORD_TYPES_VALID = ["ns", "apex", "mx", "mx_preference", "spf", "txt", "dmarc"]
---
+++ src/domain_email_records/exceptions.py
@@ -0,0 +1,2 @@
+class DomainEmailRecordsException(Exception):
+    pass
